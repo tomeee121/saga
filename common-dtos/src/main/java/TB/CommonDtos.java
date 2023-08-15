376b9564055bc68f8de0084c84ae29032406b8be
Tomasz Borowski <bortom112233@gmail.com>
2023-08-15 12:35:01 +0200

    init modules

---
+++ common-dtos/src/main/java/TB/CommonDtos.java
@@ -0,0 +1,17 @@
+package TB;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CommonDtos {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonDtos.class);
+    }
+}
